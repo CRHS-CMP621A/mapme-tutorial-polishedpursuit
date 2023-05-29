926205668f3bb2f27e6ddecd931ab87b08c1fb2a
polishedpursuit <133685570+polishedpursuit@users.noreply.github.com>
2023-05-29 13:00:29 -0300

    Update script.js

--- MapApp/script.js
+++ MapApp/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
@@ -11,13 +10,10 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-//Task 3.2 Declare map and mapEvent as global variables
 let map;
 let mapEvent;
 let workouts = [];
 
-//Task 4.1 - create the classes
-///// CLASSES ////
 class Workout {
   date = new Date();
   id = (Date.now() + "").slice(-10);
@@ -29,7 +25,6 @@ class Workout {
   }
 }
 
-//Task 4.2 - children of workout class
 class Running extends Workout {
   type = "Running";
 
@@ -40,7 +35,6 @@ class Running extends Workout {
     this.setDescription(); //sets the description for the workout title
   }
 
-  //methods
   calcPace() {
     //min / km
     this.pace = this.duration / this.distance;
@@ -49,7 +43,7 @@ class Running extends Workout {
   }
 
   setDescription() {
-    // Running on ___date____
+  
     this.description = `${this.type} on ${this.date.toDateString()}`;
   }
 }
@@ -70,19 +64,19 @@ class Cycling extends Workout {
   }
 
   setDescription() {
-    // Cycling on ___date____
+  
     this.description = `${this.type} on ${this.date.toDateString()}`;
   }
 }
 
-//Task 4.3  Testing Workout classes
+
 const run1 = new Running([39, -12], 5.2, 24, 178);
 const cycling1 = new Cycling([39, -12], 27, 95, 523);
 console.log(run1, cycling1);
 
 navigator.geolocation.getCurrentPosition(
   function (position) {
-    // console.log(position);
+   
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const coords = [latitude, longitude];
@@ -90,9 +84,7 @@ navigator.geolocation.getCurrentPosition(
     map = L.map("map").setView(coords, 13);
 
     map.on("click", function (mapE) {
-      mapEvent = mapE; // Task 3.2 - assign the mapE local variable to the global MapEvent variable
-
-      // Task 3.1 - Display user input form on the left of the screen
+      mapEvent = mapE;
       form.classList.remove("hidden");
       inputDistance.focus();
     });
@@ -102,20 +94,17 @@ navigator.geolocation.getCurrentPosition(
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    //Task 7.2 Load Existing Workouts from Local Storage
+    
     const data = JSON.parse(localStorage.getItem("workouts"));
 
-    //check if there is any data already stored
+   
     if (data) {
-      workouts = data; //load data into Workouts array
+      workouts = data; 
       console.log(data);
     }
-
-    //Task 7.3 - Render each workout to the Sidebar
-    //// Render workout in sidebar for user ////
+    
     let html;
-    for (let workout of workouts) {
-      // let d = new Date();
+    for (let workout of workouts) 
 
       if (workout.type === "Running") {
         html = `<li class="workout workout--running" data-id=${workout.id}>
@@ -175,11 +164,9 @@ navigator.geolocation.getCurrentPosition(
   }
 );
 
-// form event listener to check if submitted/completed
 form.addEventListener("submit", function (e) {
-  e.preventDefault(); //Task 3.3 prevent page reload on submit.
-
-  //Task 4.4 - get data from form and map.on
+  e.preventDefault(); 
+  
   const type = inputType.value;
   const distance = Number(inputDistance.value);
   const duration = Number(inputDuration.value);
@@ -187,34 +174,27 @@ form.addEventListener("submit", function (e) {
   const lng = mapEvent.latlng.lng;
   let workout;
 
-  //Task 4.4 continued
   if (type === "running") {
     const cadence = Number(inputCadence.value);
 
-    //validate form data later
-
-    //create new Running Object
+   
     workout = new Running([lat, lng], distance, duration, cadence);
   }
 
   if (type === "cycling") {
     const elevation = +inputElevation.value;
 
-    //Create new Cycling object
+   
     workout = new Cycling([lat, lng], distance, duration, elevation);
   }
 
-  workouts.push(workout); // add latest workout to array
-  console.log(workouts); //Testing 4.4
+  workouts.push(workout);
+  console.log(workouts); 
 
-  //Task 7.1 Local Storage of Workouts Array
+ 
   localStorage.setItem("workouts", JSON.stringify(workouts));
 
-  //Task 5.1 - render/display the workout on the sidebar
-
-  //// Render workout in sidebar for user ////
-
-  // let d = new Date();
+  
   let html;
 
   if (type === "running") {
@@ -269,7 +249,6 @@ form.addEventListener("submit", function (e) {
 
   form.insertAdjacentHTML("afterend", html);
 
-  // Task 3.3 display map marker whenever form is submitted.
   L.marker([lat, lng])
     .addTo(map)
     .bindPopup(
@@ -284,26 +263,24 @@ form.addEventListener("submit", function (e) {
     .setPopupContent("Workout")
     .openPopup();
 
-  form.reset(); // Task 3.3 challenge - set input boxes back to default
+  form.reset();
   form.classList.add("hidden");
 });
 
-//Task 3.4 - Event Listener Toggle form input type change.
 inputType.addEventListener("change", function () {
   inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
   inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
 });
 
-//Task 6 - Move map to focus on selected workout
 containerWorkouts.addEventListener("click", function (e) {
   const workoutEl = e.target.closest(".workout"); // This selects the Workout (class .workout created on submit)
 
-  if (!workoutEl) return; // if workout not found then return out of this function
+  if (!workoutEl) return; 
 
   const workout = workouts.find((work) => work.id === workoutEl.dataset.id);
 
   map.setView(workout.coords, 13, {
-    //set the Map View to the location of the workout coordinates
+    
     animate: true,
     pan: {
       duration: 1,
